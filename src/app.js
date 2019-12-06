30bcc47c5a0d817838df069ac0ef14468fecdfcc
ceci21062 <ceci21062@gmail.com>
2019-12-05 21:53:01 -0600

    libreria  cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const recruiterView =require(`./controller/recruiterView`)
+const cors = require('cors');
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(recruiterView);
